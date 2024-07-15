8271c0d0fd4a2c8d2e56e3a7b0af32e77457704a
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-07-15 22:07:01 +0530

    added borders to grid

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const container=document.querySelector('.container');
 for(let i=0;i<4;i++){
     const rowdiv=document.createElement('div');
-    rowdiv.classList.add('row',`${i}`);
+    rowdiv.classList.add('row');
+    rowdiv.setAttribute('id',`${i}`);
     for(let j=0;j<4;j++){
         const coldiv=document.createElement('div');
-        coldiv.classList.add('col',`${j}`)
+        coldiv.classList.add('col')
+        coldiv.setAttribute('id',`${j}`)
         rowdiv.appendChild(coldiv);
     }
     container.appendChild(rowdiv);
