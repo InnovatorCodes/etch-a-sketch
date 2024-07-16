d169d607e72f3890ff398430cfc8b967005f8ae0
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-07-16 13:12:49 +0530

    added functionality for incrementing opacity added button to toggle randomize colour option and incrementing opacity option

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container=document.querySelector('.container');
+let randomizeColor=false;
+let incrementing=false;
+let squares=4;
 for(let i=0;i<4;i++){
     const rowdiv=document.createElement('div');
     rowdiv.classList.add('row');
@@ -13,13 +16,20 @@ for(let i=0;i<4;i++){
 }
 container.addEventListener('mouseover',(event)=>{
     const target=event.target;
-    if(target.classList.value=='col'){
+    if(target.classList.value=='col' && !target.classList.contains('colour')){
         target.classList.add('colour');
         let r=Math.floor(Math.random()*255);
         let g=Math.floor(Math.random()*255);
         let b=Math.floor(Math.random()*255);
-        target.style.backgroundColor='rgb('+r+','+g+','+b+')';
+        if(randomizeColor) target.style.backgroundColor='rgb('+r+','+g+','+b+')';
+        if(incrementing) target.style.opacity='0.1';  
     } 
+    else if(target.classList.contains('col') && parseFloat(target.style.opacity)<1){
+        let op=parseFloat(target.style.opacity);
+        op+=0.1;
+        target.style.opacity=''+op;
+    }
+
 })
 function changeSquareNumber(num){
     while(container.firstChild){
@@ -46,6 +56,18 @@ changeButton.addEventListener('click',()=>{
     num=Number(prompt('Enter New Number of Squares per Side (max 100 squares):'));
     if(num){
         num=(num>100)?100:num;
+        squares=num;
         changeSquareNumber(num);
     }
+})
+
+const randcolour=document.querySelector('.random');
+randcolour.addEventListener('click',()=>{
+    randcolour.classList.toggle('clicked');
+    randomizeColor=!randomizeColor;
+})
+const increment=document.querySelector('.increment');
+increment.addEventListener('click',()=>{
+    increment.classList.toggle('clicked');
+    incrementing=!incrementing;
 })
